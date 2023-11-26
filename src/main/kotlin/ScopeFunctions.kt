b90255238cd83448b5e41fa0dc3162338a8e34a5
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-11-27 02:11:43 +0530

    scope function

--- src/main/kotlin/ScopeFunctions.kt
+++ src/main/kotlin/ScopeFunctions.kt
@@ -1,5 +1,33 @@
 fun main(){
-    println("hello world")
+    val employee = Employee()
+
+    val emp = employee.apply {
+        name = "a"
+        age = 20
+    }
+    println(emp)
+
+    val employee1: Employee? = null
+    employee1?.name = "kotlin"
+    employee1?.age = 5
+
+    employee1?.let {
+        it.name =  "jsk"
+        it.age = 100
+    }
+
+    val employee2 = Employee()
+    val result = with(employee2){
+        name = "qwe"
+        age = 20
+        2
+    }
+    println(result)
+
+    val employee3 = Employee()
+
+
 }
 
 
+data class Employee(var name: String = "", var age: Int = 0)
