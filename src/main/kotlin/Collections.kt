5e72b02b1ebece3d7fef8305610f839243aa7b56
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-11-29 23:12:45 +0530

    collections

---
+++ src/main/kotlin/Collections.kt
@@ -0,0 +1,30 @@
+fun main(){
+//    val day = listOf("SUN","MON","TUE","WED","THU")
+//
+//    println(day.get(0))
+//    println(day.indexOf("MON"))
+//
+//    val number = mutableListOf(1,2,3,4)
+//    println(number)
+//    number.add(5)
+//    println(number)
+//    number.removeAt(0)
+//    println(number)
+
+//    val fruits = setOf("APPLE","MANGO","BANANA",1,2,3)
+//    fruits.forEach {
+//        println(it)
+//    }
+//    for (item in fruits){
+//        println(item)
+//    }
+//    val lang = mutableSetOf("HINDI","ENGLISH","TAMIL","GUJRATI")
+//    println(lang)
+//    lang.add("BENGALI")
+//    println(lang)
+
+
+//    for (item in 0 until lang.size){
+//        println(lang.elementAt(item))
+//    }
+}
