7f839d750c67721368a4c1c3b0da40edecd2de03
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-11-30 01:00:04 +0530

    ..

---
+++ src/main/kotlin/LamdaExpression.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val msg = { println("hello")}
+    // invoking function method1
+    msg()
+    // invoking function method2
+    msg.invoke()
+
+    val oneLineLamda = {x:Int,y:Int->x+y}
+
+    println(oneLineLamda.invoke(10,20))
+
+    val student = {name:String,age:Int-> println("name is: $name age is $age") }
+
+    student.invoke("Test",15)
+
+    val employee = {
+        val name = "test"
+        val post = "SSE"
+        2
+    }
+
+    println(employee.invoke())
+}
