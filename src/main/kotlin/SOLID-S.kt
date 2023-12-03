3cb4941f348f91aafdc4df219125edcc3917fe98
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-12-03 23:20:12 +0530

    Single responsiblity

--- src/main/kotlin/SOLID-S.kt
+++ src/main/kotlin/SOLID-S.kt
@@ -1,11 +1,49 @@
 fun main(){
-
-
+    val systemManager = SystemManager()
+    systemManager.addUser("abc")
+    systemManager.deleteUser(("xyz"))
 }
 
-class Vehicle{
-    fun driveCar(){
-        println("this is car")
+
+// Single Responsibility Principle Violation
+// In this example the System class is trying to handle many different situation at the same time.
+// This approach can cause major problems in the future.
+class SystemManager{
+    fun addUser(user:String){
+        println("add $user")
     }
-    fun ride
+    fun deleteUser(user:String){
+        println("delete $user")
+    }
+    fun sendNotification(message:String){
+        println("notification: $message")
+    }
+    fun sendEmail(message:String){
+        println("email $message")
+    }
+}
+
+
+// Single Responsibility Principle Correct Usage:
+// As seen in this example, we divided our System class into specific parts
+// And placed the functions in their respective classes.
+class UserManager(){
+    fun addUser(user:String){
+        println("add $user")
+    }
+    fun deleteUser(user:String){
+        println("delete $user")
+    }
+}
+
+class NotificationManager(){
+    fun sendNotification(message:String){
+        println("notification: $message")
+    }
+}
+
+class EmailManager(){
+    fun sendEmail(message:String){
+        println("email $message")
+    }
 }
