e952544002aa51c82ba8bc55db126f0859e7564b
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-12-10 23:15:02 +0530

    sealed-class

--- src/main/kotlin/Sealed.kt
+++ src/main/kotlin/Sealed.kt
@@ -1,31 +1,28 @@
 // A sealed class with a string property
-sealed class Fruit(val x : String)
-{
+sealed class Fruit(val x: String) {
     // Two subclasses of sealed class defined within
     class Apple : Fruit("Apple")
     class Mango : Fruit("Mango")
 }
 
 // A subclass defined outside the sealed class
-class Pomegranate: Fruit("Pomegranate")
+class Banana : Fruit("Banana")
 
 // A function to take in an object of type Fruit
 // And to display an appropriate message depending on the type of Fruit
-fun display(fruit: Fruit)
-{
-    when(fruit)
-    {
+fun display(fruit: Fruit) {
+    when (fruit) {
         is Fruit.Apple -> println("${fruit.x} is good for iron")
         is Fruit.Mango -> println("${fruit.x} is delicious")
-        is Pomegranate -> println("${fruit.x} is good for vitamin d")
+        is Banana -> println("${fruit.x} is good for vitamin d")
     }
 }
-fun main()
-{
+
+fun main() {
     // Objects of different subclasses created
     val obj = Fruit.Apple()
     val obj1 = Fruit.Mango()
-    val obj2 = Pomegranate()
+    val obj2 = Banana()
 
     // Function called with different objects
     display(obj)
