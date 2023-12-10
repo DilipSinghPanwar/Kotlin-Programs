17164818a499c4686632988fee7446598b1e1c5e
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-12-10 08:20:34 +0530

    map

--- src/main/kotlin/Map.kt
+++ src/main/kotlin/Map.kt
@@ -1,7 +1,5 @@
 fun main(){
-    val list = mutableListOf(1,2,3,4,5)
-    list.map {
-        println(it * it)
-    }
-    println(list)
+    val numbers = mutableListOf(1,2,3,4,5)
+    val squaredNumbers = numbers.map { it * it }
+    println(squaredNumbers)
 }
