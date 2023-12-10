921555329c4151da09606014ef398b6996f3939a
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-12-11 00:11:56 +0530

    enum

---
+++ src/main/kotlin/Enum.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println(Days.SUNDAY)
+    println(Days.MONDAY)
+    println(Days.TUESDAY)
+    println(Days.WEDNESDAY)
+}
+enum class Days{
+    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+}
