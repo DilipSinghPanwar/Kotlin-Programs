c414d1271ab13c560df6fe2f3a4e60d8ade2a6e6
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-12-10 10:15:13 +0530

    inline

--- src/main/kotlin/Inline.kt
+++ src/main/kotlin/Inline.kt
@@ -1,3 +1,19 @@
-fun main(){
+fun main() {
+//    print2Table()
+    val fn = { x: Int ->
+        for (i in 1..10) {
+            println("2 * $i = " + i * x)
+        }
+    }
+    print2Table1(2, fn)
+}
 
+inline fun print2Table1(input: Int, fn: (x: Int) -> Unit) {
+    fn.invoke(input)
+}
+
+fun print2Table() {
+    for (i in 1..10) {
+        println(2 * i)
+    }
 }
